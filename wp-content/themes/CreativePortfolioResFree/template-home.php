90176abf7d3c5f4c0d62c16eb5bcf1356a41b3a8
Edward <graphicartgeek@gmail.com>
2018-01-29 21:25:05 -0700

    home page template

--- wp-content/themes/CreativePortfolioResFree/template-home.php
+++ wp-content/themes/CreativePortfolioResFree/template-home.php
@@ -2,7 +2,6 @@
 /**
  * Template Name: Home page
  */
-
  get_header(); ?>	
 <?php $shortname = "unit_theme"; ?>
 <div id="slideshow_cont">
@@ -48,46 +47,20 @@
 <div id="content">
 	<div class="container">
 		<?php
-		$category_ID = get_category_id('blog');
-		$args = array(
-			 'post_type' => 'post',
-			 'posts_per_page' => 6,
-			 'post__not_in' => $slider_arr,
-			 'cat' => '-' . $category_ID
-			 );
-		query_posts($args);
-		$x = 0;
-		while (have_posts()) : the_post(); ?>                        					
-			<div class="home_box">
-				<div class="home_box_media">
-					<?php if(get_post_meta( get_the_ID(), 'page_featured_type', true ) == 'youtube') { ?>
-						<iframe width="560" height="315" src="http://www.youtube.com/embed/<?php echo get_post_meta( get_the_ID(), 'page_video_id', true ); ?>" frameborder="0" allowfullscreen></iframe>
-					<?php } elseif(get_post_meta( get_the_ID(), 'page_featured_type', true ) == 'vimeo') { ?>
-						<iframe src="http://player.vimeo.com/video/<?php echo get_post_meta( get_the_ID(), 'page_video_id', true ); ?>?title=0&amp;byline=0&amp;portrait=0&amp;color=03b3fc" width="500" height="338" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-					<?php } else { ?>
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('ut-home-image'); ?></a>
-						
-						
-						<a class="home_box_hover" href="<?php the_permalink(); ?>">
-							<div class="home_box_row">
-								<div class="home_box_cell">
-									<?php echo ds_get_excerpt('200'); ?>
-								</div> <!-- //home_box_cell -->
-							</div> <!-- //home_box_row -->
-						</a> <!-- //home_box_hover -->
-						
-					<?php } ?>			
-				</div> <!-- //home_box_media -->	
-				
-										
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				  <p class="cat"><?php the_category(' / '); ?></p>
-				
-			</div> <!-- //home_box -->		
-			<?php if($x == 1) { echo '<div class="clear"></div>'; $x = -1; } ?>
-		<?php $x++; ?>
-		<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
+
+			$categories = get_categories();
+			foreach( $categories as $category ) {
+				$cat_image = get_field('category_image', $category);
+				if($cat_image){ ?>
+				<div class="home_box">
+					<div class="home_box_media">
+					<a href="<?php echo get_category_link($category->term_id); ?>"><img src="<?php echo $cat_image['sizes']['ut-home-image']; ?>" /></a>
+					<h3><a href="<?php echo get_category_link($category->term_id); ?>"><?php echo $category->name; ?></h3>
+					</div>
+				</div>
+				<?php }				
+			}
+		?>
 		<div class="clear"></div>
 		
 		
